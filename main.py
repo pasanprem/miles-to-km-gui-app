1d8812fe74b2e33d279deb0ea345c072ed4887df
pasanprem <46164432+pasanprem@users.noreply.github.com>
2023-03-22 12:02:09 +0000

    Primary requirements completed.

--- main.py
+++ main.py
@@ -15,22 +15,37 @@ equal_label = tkinter.Label(text="is equal to")
 equal_label.grid(column=1, row=2)
 equal_label.config(padx=10,pady=10)
 
+
+# Functions
+def button_clicked():
+    mile_value = float(miles_input.get())
+    km_value = mile_value * 1.6
+    kmresult_label.config(text=km_value)
+
+
 # Input for Miles
 miles_input = tkinter.Entry(width=10)
 miles_input.grid(column=2,row=1)
 # miles_input.config(padx=10,pady=10) // This generates an error
 
+# Label to say 'Miles'
+Miles_label = tkinter.Label(text="Miles")
+Miles_label.grid(column=3,row=1)
+
 # Label to display KM value
+kmresult_label = tkinter.Label(text="0.00")
+kmresult_label.grid(column=2,row=2)
 
 
 
 # Label to say 'KM'
-
+KM_label = tkinter.Label(text="KM")
+KM_label.grid(column=3,row=2)
 
 
 # Button to 'Calculate'
-
-
+calc_button = tkinter.Button(text="Calculate", command=button_clicked)
+calc_button.grid(column=2,row=3)
 
 # Stay and Exit
 window.mainloop()
